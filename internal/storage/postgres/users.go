81e5c7670f0ec59eee2f10e194186ebc9dae866f
Onnywrite <vlad.mokeev161106@gmail.com>
2024-08-16 01:33:41 +0300

    refactor: migrated to squirrel

--- internal/storage/postgres/users.go
+++ internal/storage/postgres/users.go
@@ -3,19 +3,21 @@ package postgres
 import (
 	"context"
 
+	"github.com/Masterminds/squirrel"
 	"github.com/Onnywrite/ssonny/internal/domain/models"
 	"github.com/Onnywrite/ssonny/internal/lib/cuteql"
 	"github.com/Onnywrite/ssonny/internal/storage/repo"
 )
 
 func (pg *PgStorage) SaveUser(ctx context.Context, user models.User) (*models.User, repo.Transactor, error) {
-	return cuteql.GetNamed[models.User, models.User](ctx, pg.db, nil, `
-		INSERT INTO users 
-			(user_nickname, user_email, user_verified,
-			user_gender, user_password_hash, user_birthday)
-		VALUES
-			(:user_nickname, :user_email, :user_verified,
-			:user_gender, :user_password_hash, :user_birthday)
-		RETURNING *
-	`, user)
+	return cuteql.GetSquirreled[models.User](ctx, pg.db, nil,
+		squirrel.
+			Insert("users").
+			Columns(
+				"user_nickname", "user_email", "user_verified",
+				"user_gender", "user_password_hash", "user_birthday").
+			Values(user.Nickname, user.Email, user.IsVerified,
+				user.Gender, user.PasswordHash, user.Birthday).
+			Suffix("RETURNING *"),
+	)
 }
